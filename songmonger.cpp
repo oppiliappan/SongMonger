dd221e0ce80db8a50f9558cac4bc497cb22ed7e5
NerdyPepper <akshayoppiliappan@gmail.com>
2017-10-04 19:35:24 +0530

    More function definitions

--- songmonger.cpp
+++ songmonger.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
@@ -8,15 +9,32 @@ private:
 	char title[20], artist[10], album[10];
 	int isfav, duration, views;
 public:
+	Song(){
+		strcpy(title, "");
+		strcpy(album, "");
+		strcpy(artist, "");
+		duration = 0;
+		isfav = 0;
+	}
 	void dispData();
 	void favit();
 };
+
+void Song::dispData(){
+	cout<<title<<"\t"<<artist<<"\t"<<album<<"\t"<<duration;
+}
+
+void Song::favit(){
+	if(isfav == 0) isfav = 1;
+	else isfav = 0;
+}
 
 class Playlist{
 private:
 	char playname[20];
 	Song songlist[10];
 public:
+	void dispPlayName();
 	void dispSongs();
 	void addSongs();
 	static int songcount;
@@ -35,19 +53,24 @@ void Playlist::addSongs(){
 
 class User{
 private:
-	char username[10], language[10];
-	Playlist play;
+	char username[10];
+	Playlist play[10];
 public:
 	void dispDetails();
 	void addPlaylist();
 	void viewPlaylist(char);
+
+	static int playcount;
 }user1, user2, user3;
+
+int User::playcount = 0;
 
 void User::dispDetails(){
 	cout<<"\nName: "<<username;
-	cout<<"\nLanguage: "<<language;
 	cout<<"\nPlaylists: ";
-	play.dispSongs();
+	for(int i=0; i<playcount; i++){
+		play[i].dispPlayName();
+	}
 }
 
 
