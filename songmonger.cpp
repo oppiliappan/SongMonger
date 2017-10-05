7df47a49dd28e396be1bed195a5fbf7983e4dcff
synecducky <attugattu@gmail.com>
2017-10-05 16:40:51 +0530

    Added dispPlayName(), addSong(), Commenting Culture

--- songmonger.cpp
+++ songmonger.cpp
@@ -5,21 +5,22 @@
 
 using namespace std;
 
+// How about making this a queue?
 class Song{
 private:
 	char title[20], artist[10], album[10];
 	int isfav, duration, views;
 public:
 	Song(){
-		strcpy(title, "");
-		strcpy(album, "");
-		strcpy(artist, "");
+		strcpy(title, "UNTITLED");
+		strcpy(album, "UNTITLED");
+		strcpy(artist, "NIL");
 		duration = 0;
 		isfav = 0;
 	}
-	void dispData();
-	void favit();
-	void editData(int);
+	void dispData(); //Done bare minimum
+	void favit(); // Done
+	void editData(int); // INCOMPLETE
 };
 
 void Song::dispData(){
@@ -31,8 +32,10 @@ void Song::favit(){
 	else isfav = 0;
 }
 
+// Not sure what exactly you were trying here
+// I'll leave it for you to do
 void Song::editData(int x){
-	if(x == 1){
+	if(x == 1) {
 		cout<<"\nEnter new title: ";
 	}
 }
@@ -42,23 +45,45 @@ private:
 	char playname[20];
 	Song songlist[10];
 public:
-	void dispPlayName();
-	void dispSongs();
-	void addSongs();
+	Playlist() {
+		strcpy(playname, "UNTITLED");
+	}
+	void dispPlayName(); // complete
+	void dispSongs(); // complete
+
+	// depends on an incomplete function
+	void addSong(); //Songs --> Song because one song at a time
+
 	static int songcount;
 };
 
 int Playlist::songcount = 0;
 
-void Playlist::dispSongs(){
+void Playlist::dispPlayName() {
+	puts(playname);
+	cout << "\t\t" << songcount << "Song(s)";
+}
+
+void Playlist::dispSongs() {
 	for(int i=0; i<Playlist::songcount; i++){
 		songlist[i].dispData();
 	}
 }
-void Playlist::addSongs(){
 
+// Is a recursive function
+// Depends on an incomplete function
+void Playlist::addSong() {
+	char ch;
+	songlist[songcount].editData(); // NOTE: Assuming this also adds data
+	songcount++;
+	cout << "Add more? [Y/n] ";
+	cin >> ch;
+	if (ch == 'y') {
+		addSong();
+	}
 }
 
+// Generally incomplete
 class User{
 private:
 	char username[10];
