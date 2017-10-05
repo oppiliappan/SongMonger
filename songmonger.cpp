a0b61a17821716e982e6fec37868c61261e95a2d
NerdyPepper <akshayoppiliappan@gmail.com>
2017-10-05 19:35:21 +0530

    Finish editData(), Add new function addData()

--- songmonger.cpp
+++ songmonger.cpp
@@ -18,25 +18,55 @@ public:
 		duration = 0;
 		isfav = 0;
 	}
+
+	void addData();
 	void dispData(); //Done bare minimum
 	void favit(); // Done
 	void editData(int); // INCOMPLETE
 };
 
+// Enter song data for the first time
+void Song::addData(){
+	cout<<"\nEnter title:";
+	cout<<"\nEnter artist: ";
+	cout<<"\nEnter album: ";
+	cout<<"\nEnter duration: ";
+}
+// Display formatted song data
 void Song::dispData(){
 	cout<<title<<"\t"<<artist<<"\t"<<album<<"\t"<<duration;
 }
 
+// Favorite your songs
 void Song::favit(){
 	if(isfav == 0) isfav = 1;
 	else isfav = 0;
 }
 
-// Not sure what exactly you were trying here
-// I'll leave it for you to do
+// You can choose what part of a song you want to edit with numbers
+// 1 for title
+// 2 for Album
+// 3 for Artist
+// 4 for Duration
+// This function is probably useless
 void Song::editData(int x){
 	if(x == 1) {
 		cout<<"\nEnter new title: ";
+	}
+	else if(x == 2) {
+		cout<<"\nEnter new album name: ";
+	}
+	else if(x == 3) {
+		cout<<"\nEnter new artist name: ";
+	}
+	else if(x == 4) {
+		cout<<"\nEnter new duration: ";
+	}
+	else {
+		cout<<"\nInvalid option. Try again";
+		cout<<"\nEnter the option";
+		cin>>x;
+		editData(x);
 	}
 }
 
@@ -71,10 +101,10 @@ void Playlist::dispSongs() {
 }
 
 // Is a recursive function
-// Depends on an incomplete function
+// Depends on addData()
 void Playlist::addSong() {
 	char ch;
-	songlist[songcount].editData(); // NOTE: Assuming this also adds data
+	songlist[songcount].addData();
 	songcount++;
 	cout << "Add more? [Y/n] ";
 	cin >> ch;
@@ -87,7 +117,7 @@ void Playlist::addSong() {
 class User{
 private:
 	char username[10];
-	Playlist play[10];
+	Playlist play[10];// Can users have songs which dont belong to any playlist?
 public:
 	void dispDetails();
 	void addPlaylist();
