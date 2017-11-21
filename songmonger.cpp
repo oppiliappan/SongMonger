290877a330a8b6b1827b6b792011498941d0b76b
nerdypepper <akshayoppiliappan@gmail.com>
2017-11-21 12:12:35 +0530

    Atharva version

--- songmonger.cpp
+++ songmonger.cpp
@@ -2,41 +2,45 @@
 #include <fstream>
 #include <string.h>
 #include <stdlib.h>
+#include <cstdlib>
 using namespace std;
 
 // How about making this a queue?
 class Song{
-private:
-	char title[20], artist[10], album[10];
-	int isfav, duration, views;
-public:
-	Song(){
-		strcpy(title, "UNTITLED");
-		strcpy(album, "UNTITLED");
-		strcpy(artist, "NIL");
-		duration = 0;
-		isfav = 0;
-	}
+	private:
+		char title[20], artist[10], album[10];
+		int isfav, duration, views;
+	public:
+		Song() {
+			strcpy(title, "UNTITLED");
+			strcpy(album, "UNTITLED");
+			strcpy(artist, "NIL");
+			duration = 0;
+			isfav = 0;
+		}
 
-	void addData();
-	void dispData(); //Done bare minimum
-	void favit(); // Done
-	void editData(int); // INCOMPLETE
+		void addData();
+		void dispData(); //Done bare minimum
+		void favit(); // Done
+		void editData(); //Done for now
 };
 
 // Enter song data for the first time
 void Song::addData(){
-	cout<<"\nEnter title:";
-	fgets(title, 20, stdin);
-	cout<<"\nEnter artist: ";
-	fgets(artist, 20, stdin);
-	cout<<"\nEnter album: ";
-	fgets(album, 20, stdin);
-	cout<<"\nEnter duration (sec.): ";
+	cout<<"\nEnter title: ";
+	//fgets(title, 20, stdin);
+	cin>>title;
+	cout<<"Enter artist: ";
+	//fgets(artist, 20, stdin);
+	cin>>artist;
+	cout<<"Enter album: ";
+	//fgets(album, 20, stdin);
+	cin>>album;
+	cout<<"Enter duration (sec.): ";
 	cin>>duration;
 }
 // Display formatted song data
-void Song::dispData(){
+void Song::dispData() {
 	cout<<title<<"\t"<<artist<<"\t"<<album<<"\t"<<duration;
 	if(isfav == 1)cout<<"\t*\n";
 	else cout<<"\n";
@@ -53,40 +57,44 @@ void Song::favit(){
 // 2 for Album
 // 3 for Artist
 // 4 for Duration
-// This function is probably useless
-void Song::editData(int x){
-	if(x == 1) {
+// This function is probably useless --> How on earth was it incomplete for so long?!
+// Illlogical to change duration of a song
+void Song::editData(){
+	int *ch = new int;
+	cout << "Edit options: \n1. Title\n2. Album Name\n3. Artist name\n";
+	cin >> *ch;
+	cin.ignore();
+	if(*ch == 1) {
 		cout<<"\nEnter new title: ";
-	} else if(x == 2) {
+		fgets(title, 20, stdin);
+	} else if(*ch == 2) {
 		cout<<"\nEnter new album name: ";
-	} else if(x == 3) {
+		fgets(album, 20, stdin);
+	} else if(*ch == 3) {
 		cout<<"\nEnter new artist name: ";
-	} else if(x == 4) {
-		cout<<"\nEnter new duration: ";
+		fgets(artist, 20, stdin);
 	} else {
-		cout<<"\nInvalid option. Try again";
-		cout<<"\nEnter the option";
-		cin>>x;
-		editData(x);
+		cout<<"\nInvalid option. Try again\n";
+		editData();
 	}
 }
 
 class Library{
-protected:
-	Song songlist[20];
-public:
-	void dispSongs(); // complete
+	protected:
+		Song songlist[20];
+	public:
+		void dispSongs(); // complete
 
-	// depends on an incomplete function
-	void addSong(); //Songs --> Song because one song at a time
-	void delSong(); // New and done
-	static int songcount;
+		// depends on an incomplete function
+		void addSong(); //Songs --> Song because one song at a time
+		void delSong(); // New and done
+		void editSong();
+		int songcount;
 };
 
-int Library::songcount = 0;
-
 void Library::dispSongs() {
-	for(int i=0; i<Library::songcount; i++){
+	cout << "\n";
+	for(int i=0; i<songcount; i++){
 		cout << i + 1 << ". ";
 		songlist[i].dispData();
 	}
@@ -98,9 +106,18 @@ void Library::addSong() {
 	do {
 		songlist[songcount].addData();
 		songcount++;
-		cout << "Add more? [Y/n] ";
+		cout << "Add more? [y/n] ";
 		cin >> ch;
 	} while(ch == 'y');
+}
+
+void Library::editSong() {
+	int *ch = new int;
+	cout << "Which song to edit?";
+	dispSongs();
+	cout << "Select number: ";
+	cin >> *ch;
+	songlist[(*ch - 1)].editData();
 }
 
 void Library::delSong() {
@@ -117,16 +134,16 @@ void Library::delSong() {
 
 // Inheriting Class
 class Playlist: public Library {
-private:
-	char playname[20];
-public:
-	Playlist() {
-		strcpy(playname, "UNTITLED");
-	}
-	void setPlayName();
-	void dispPlayName(); // complete
-	void addSong();
-	void delSong();
+	private:
+		char playname[20];
+	public:
+		Playlist() {
+			strcpy(playname, "UNTITLED");
+		}
+		void setPlayName();
+		void dispPlayName(); // complete
+		void addSong();
+		void delSong();
 };
 
 void Playlist::setPlayName() {
@@ -168,27 +185,44 @@ void Playlist::delSong() {
 }
 
 class User {
-private:
-	char username[10];
-	Library songs;
-	Playlist plists[10];
+	private:
+		char username[10];
+		Library songs;
+		Playlist plists[10];
 
-public:
-	User() {
-		strcpy(username, "Jawn_dough");
-		isactivated = 'n';
-	}
-	void getUsername(){
-		cout<<username;
-	}
-	void setup(); // for the first time setup
-	void dispDetails();
-	void addToLibrary();
-	void viewLibrary();
-	void delFromLibrary();
-	static int usercount; // this is the static variable we deserve
-	int playlistcount;
-	char isactivated; // Self explanatory - high quality comment
+	public:
+		User() {
+			strcpy(username, "Jawn_dough");
+			isactivated = 'n';
+		}
+		void getUsername(){
+			cout<<username; // dont add spaces here, add them where you need them
+		}
+		void setup(); // for the first time setup
+		void dispAll();
+
+		// Library function definitions
+		void addToLibrary();
+		void editLibrary();
+		void viewLibrary();
+		void delFromLibrary();
+
+		// Playlist function definitions
+		void dispPlayName();
+		void editPlaylistName();
+		void delPlaylist();
+		static int usercount; // this is the static variable we deserve
+
+		// File handling
+		void storeUsercount();
+		void readUserCount();
+		void storeUserdata();
+		void readUserdata();
+		void storeAll(User *userobj);
+		void readAll(User *userobj);
+
+		int playlistcount;
+		char isactivated; // Self explanatory - high quality comment
 }users[5];
 
 int User::usercount = 0;
@@ -201,13 +235,34 @@ void User::setup() {
 	cout << "Now let's get songmongering!";
 }
 
-void User::dispDetails() {
+void User::dispAll() {
 	cout<<"\nName: "<<username;
-	cout<<"\nSongs: \n";
-	if ((Library::songcount) == 0) cout<<"No songs\n";
+	cout<<"\nSongs: ";
+	if ((songs.songcount) == 0) cout<<"\nNo songs";
 	else{
 		songs.dispSongs();
 	}
+	dispPlayName();
+}
+
+void User::addToLibrary() {
+	songs.addSong();
+	cout<<"\nYou currently have "<<songs.songcount<<" songs\n";
+}
+
+void User::editLibrary() {
+	songs.editSong();
+}
+
+void User::viewLibrary(){
+	songs.dispSongs();
+}
+
+void User::delFromLibrary() {
+	songs.delSong();
+}
+
+void User::dispPlayName() {
 	cout<<"\nPlaylists: \n";
 	if (User::playlistcount == 0) cout<<"No playlists\n";
 	else{
@@ -217,19 +272,66 @@ void User::dispDetails() {
 	}
 }
 
-void User::addToLibrary() {
-	songs.addSong();
-	cout<<"\nYou currently have "<<Library::songcount<<" songs\n";
+void User::editPlaylistName() {
+	int *ch = new int;
+	cout << "Which playlist?";
+	dispPlayName();
+	cin >> *ch;
+	plists[*ch].setPlayName();
 }
 
-void User::viewLibrary(){
-	songs.dispSongs();
+void User::delPlaylist() {
+	int *ch = new int;
+	cout << "Which playlist?";
+	dispPlayName();
+	cin >> *ch;
+	for (int i = *ch; i < playlistcount; i++) {
+		if(i < sizeof(Playlist)/2) plists[i] = plists[i + 1]; // if statement prevents segmentation faults
+	}
+	playlistcount--;
 }
 
-void deleteSong(){
-	songs.delSong();
+void User::storeUsercount() {
+    remove("usercount.dat");
+    ofstream countfile;
+    countfile.open("usercount.txt");
+    countfile << User::usercount;
 }
 
+void User::readUserCount() {
+    ifstream countfile;
+    countfile.open("usercount.txt");
+    countfile >> User::usercount;
+}
+
+void User::storeUserdata() {
+	ofstream file;
+	file.open("userdata.dat", ios::out | ios::binary | ios::app);
+	file.write((char*)this, sizeof(User));
+}
+
+void User::readUserdata() {
+	ifstream file;
+	file.open("userdata.dat", ios::in | ios::binary | ios::app);
+	file.read((char*)this, sizeof(User));
+}
+
+// This stores data for ALL USERS
+void User::storeAll(User *userobj) {
+	remove("userdata.dat");
+	for (int i = 0; i < User::usercount; i++) {
+		userobj[i].storeUserdata();
+	}
+	storeUsercount();
+}
+
+// Loads all data into User objects
+void User::readAll(User *userobj) {
+    readUserCount();
+	for (int i = 0; i < User::usercount; i++) {
+		userobj[i].readUserdata();
+	}
+}
 
 /* -------------------------- MAIN ----------------------------- */
 int main(){
@@ -241,31 +343,31 @@ int main(){
 	1. User abc
 	2. User xyz
 	Choose option
-		Welcome User abc
-			Name: abc
-			Songs:
-				1.
-				2.
-				3.
-			Playlists:
-				1.
-				2.
-				3.
-			1. Library actions
-			2. Playlist actions
-			3. Logout
-			Enter option
-				Library actions
-					1. Add song
-					2. Edit song
-					3. Delete song
-					4. Back
+	Welcome User abc
+		Name: abc
+		Songs:
+		1.
+		2.
+		3.
+		Playlists:
+		1.
+		2.
+		3.
+		1. Library actions
+		2. Playlist actions
+		3. Logout
+		Enter option
+		Library actions
+		1. Add song
+		2. Edit song
+		3. Delete song
+		4. Back
 
-					Playlist actions
-					1. Create new playlist
-					2. Edit existing playlist
-					3. Delete playlist
-					4. Back
+		Playlist actions
+		1. Create new playlist
+		2. Edit existing playlist
+		3. Delete playlist
+		4. Back
 	*/
 
 	// enter user select
@@ -287,24 +389,32 @@ int main(){
 			users[i].setup();
 		}
 
-		cout<<"Select user\n";
-		for(int i=0; i<users[0].usercount; i++){
+		users[0].readAll(users); // loads all users
+        cout << "Currently Active Users: " << User::usercount;
+		for(int i = 0; i < User::usercount; i++) {
 			// this will make sure only existing users show up
-			cout << '\n' << i + 1 << " ";
+			cout << "\n" << i + 1 << ". ";
 			users[i].getUsername();
+			cout<<"\n";
 		}
 
 		int choose_user = -1;
-		cin>>choose_user;
+		cout<<"\nSelect user\n";
+		cin >> choose_user;
 		choose_user--; // convert to array index
 
 		// enter user actions
 		char logout = 'n';
 		do{
-			users[choose_user].dispDetails();
+			system("clear");
+			users[choose_user].dispAll();
+
+			// these are 'user actions'
+			cout<<"\n-------MAIN MENU-------\n";
 			cout<<"1. Library actions\n";
 			cout<<"2. Playlist actions\n";
-			cout<<"3. Back to user select\n";
+			cout<<"3. User Details\n";
+			cout<<"4. Back to user select\n";
 			cout<<"\n What would you like to do?\n";
 
 			int user_action;
@@ -312,80 +422,90 @@ int main(){
 
 			// enter specific actions
 			char exit_actions='n';
+			char quit_lib_actions = 'n';
+			char quit_play_actions = 'n';
 			do{
-				switch(user_action){
+				switch(user_action) {
 					case 1:{
-
+							system("clear");
 						// enter lib actions
-						char quit_lib_actions = 'n';
-						do{
-							cout<<"\nLibrary actions\n";
-							cout<<"1. Add songs\n";
-							cout<<"2. Edit songs\n";
-							cout<<"3. Delete songs\n";
-							cout<<"4. Back\n";
+							do{
+								cout<<"\n-------Library actions-------\n";
+								cout<<"1. Add songs\n";
+								cout<<"2. Edit songs\n";
+								cout<<"3. Delete songs\n";
 
-							int lib_action;
-							cin>>lib_action;
+								int lib_action;
+								cin>>lib_action;
 
-							switch(lib_action){
-								case 1:
-									users[choose_user].addToLibrary();
-									break;
-								case 2:
-									// aaaaaa we dont have an edit function
-									break;
-								case 3:
-									users.[choose_user].delFromLibrary();
-									break;
-								case 4:
-									quit_lib_actions = 'y';
-									break;
-								default:
-									cout<<"Invalid option\n";
-									break;
-							}
-							cout<<"Go back? (y/n)\n";
-							cin>>quit_lib_actions;
-						} while (quit_lib_actions == 'n');
-						break;
+								switch(lib_action){
+									case 1:
+										users[choose_user].addToLibrary();
+										break;
+									case 2:
+										users[choose_user].editLibrary();
+										break;
+									case 3:
+										users[choose_user].delFromLibrary();
+										break;
+									default:
+										cout<<"Invalid option\n";
+								}
+
+								cout<<"Would you like to quit library actions? (y/n)\n";
+								cin>>quit_lib_actions;
+							} while (quit_lib_actions == 'n');
+							break;
 					}
 					case 2:{
+							system("clear");
+							// enter play actions
+							do{
+								cout<<"-------\nPlaylist actions-------\n";
+								cout<<"1. Create playlist\n";
+								cout<<"2. Edit existing playlist\n";
+								cout<<"3. Delete playlist\n";
 
-						// enter play actions
-						char quit_play_actions = 'n';
-						do{
-							cout<<"\nPlaylist actions\n";
-							cout<<"1. Create playlist\n";
-							cout<<"2. Edit existing playlist\n";
-							cout<<"3. Delete playlist\n";
-							int play_action;
-							cin>>play_action;
+								int play_action;
+								cin>>play_action;
 
-							switch(play_action){
-								case 1:
-									users[choose_user].addToLibrary();
-									break;
-								case 2:
-									// aaaaaa we dont have an edit function
-									break;
-								case 3:
-									break;
-									// we dont have a delete function either aaaaaa
-								case 4:
-									quit_play_actions = 'y';
-									break;
-								default:
-									cout<<"Invalid option\n";
-							}
-							cout<<"Would you like to quit? (y/n)\n";
-							cin>>quit_play_actions;
-						} while (quit_play_actions == 'n');
+								switch(play_action){
+									case 1:
+										users[choose_user].addToLibrary();
+										break;
+									case 2:
+										users[choose_user].editPlaylistName();
+										break;
+									case 3:
+										users[choose_user].delPlaylist();
+										break;
+									default:
+										cout<<"Invalid option\n";
+								}
+
+								cout<<"Would you like to quit playlist actions? (y/n)\n";
+								cin>>quit_play_actions;
+							} while (quit_play_actions == 'n');
+						break;
+					}
+
+					case 3:{
+						users[choose_user].dispAll();
 						break;
 					}
 				}
-			} while (exit_actions == 'n');
-		} while (logout == 'n');
-	} while (quit_program == 'n');
+				users[0].storeAll(users);
+				cout<<"Get back to that actions thing?\n(y/n)";
+				cin>>exit_actions;
+			} while (exit_actions == 'n'); //back to 2 choice menu
+
+			cout<<"Logout of ";
+			users[choose_user].getUsername();
+			cout<<" ?\n";
+		} while (logout == 'n'); //back to user select
+
+		cout<<"Would you like to quit the program?(y/n)\n";
+		cin>>quit_program;
+	} while (quit_program == 'n'); //back to your PC
 	return 0;
 }
